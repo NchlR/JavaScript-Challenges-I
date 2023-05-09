8bb32481809a13dc6b4c524bb467b55822add768
Nico <jndr051701@gmail.com>
2023-05-09 11:03:39 -0600

    notes

---
+++ app.js
@@ -0,0 +1,90 @@
+// Return the Sum of Two Numbers
+// function addition(num1, num2) {
+//    return num1 + num2
+// }
+
+// console.log(addition(3,5));
+
+// =================================================================
+
+// Convert Hours into Seconds
+// function hoursintoSeconds(hour) {
+// let minutes = hour * 60
+// let seconds = minutes * 60
+
+// or
+
+// let seconds = hour * 60 * 60
+// return seconds
+
+// or
+
+// return hour * 60 * 60;
+// }
+
+// console.log(hoursintoSeconds(2))
+
+// =================================================================
+
+// function calcPerimeter(length, width) {
+// return 2 * length + 2 * width;
+
+// or
+
+// return 2 * (length + width)
+// }
+
+// console.log(calcPerimeter(10,20));
+
+// =================================================================
+
+// Calculate the Area of a Triangle
+// function calcAreaOfTriangle(base, height) {
+// return 0.5 * base * height;
+// }
+
+// console.log(calcAreaOfTriangle(20, 20));
+
+// =================================================================
+
+// Extend a String
+
+// function appendFrontend(string) {
+// return string + 'Frontend';
+// }
+
+// console.log(appendFrontend('Apple'))
+
+// =================================================================
+
+// Greater than 100?
+// function sumGreaterThan100(num1, num2) {
+// if (num1 + num2 > 100) {
+//    return true;
+// }
+// else {
+//    return false;
+// }
+
+// or can be simplified to
+
+// return num1 + num2 > 100
+
+// true or false built into code
+// }
+
+// console.log(sumGreaterThan100(91, 10));
+
+// =================================================================
+
+//Less than or Equal to Zero
+function lessThanOrEqualToZero(num1) {
+    return num1 <= 0
+}
+
+console.log(lessThanOrEqualToZero(3));
+
+
+
+
+
